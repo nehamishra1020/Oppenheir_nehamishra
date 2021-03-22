79d9cf974de716e043f9fff85b3e9a674cda10a5
nehamishra1020 <57174804+nehamishra1020@users.noreply.github.com>
2021-03-22 16:50:08 +0800

    Add files via upload

--- src/test/java/testrunner/Runner.java
+++ src/test/java/testrunner/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\LOKESH\\eclipse-workspace\\Logintest\\src\\test\\java"
+@CucumberOptions(features="C:\\Users\\LOKESH\\eclipse-workspace\\Oppenheimer_NehaMishra\\src\\test\\java"
 ,glue={"stepdeftest"
 },
 plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
